d37038d6d9b5b844b64cef857ec5efc74ed7442c
S. M. Abrar Jahin <abrarjahin@live.com>
2016-07-17 16:42:07 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+var chart;
+$(document).ready(function()
+{
+	chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'container',
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
+		title: {
+			text: 'Browser market shares at a specific website, 2010'
+		},
+		tooltip: {
+			formatter: function() {
+				return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+			}
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					color: '#000000',
+					connectorColor: '#000000',
+					formatter: function() {
+						return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+					}
+				}
+			}
+		},
+		series: [{
+			type: 'pie',
+			name: 'Browser share',
+			data: [
+				['Firefox',   45.0],
+				['IE',       26.8],
+				{
+					name: 'Chrome',
+					y: 12.8,
+					sliced: true,
+					selected: true
+				},
+				['Safari',    8.5],
+				['Opera',     6.2],
+				['Others',   0.7]
+			]
+		}]
+	});
+
+	$("#button").on("click", function()
+	{
+		chart.series[0].setData([
+			['number1',Math.random()*1000],
+			['number2',Math.random()*1000],
+			['number3',Math.random()*1000],
+			['number4',Math.random()*1000],
+			['number5',Math.random()*1000],
+			['number6',Math.random()*1000],
+			{
+				name: 'number7',
+				y: Math.random()*1000,
+				sliced: true,
+				selected: true
+			},
+		], true);
+	});
+});
